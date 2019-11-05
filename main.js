967054e4e437e8274d36aaa187180cd3f5e41c32
FlorianSdorra <fsd114@posteo.de>
2019-11-05 12:49:07 +0100

    works perfect

--- main.js
+++ main.js
@@ -2,31 +2,21 @@ $(document).ready(function () {
     console.log("document is ready...");
     $('.addBtn').click(function () {
         let val = $('input').val();
-        let html = '<li>' + val + '<button class="close">x</button></li>'
+        let html = '<li>' + val + '<button class="close">x</button></li>';
 
-        $(html).appendTo('ul');
-        $('input').val('');
+        $(html).appendTo('ul')
+        $('input').val('')
     });
 
     $('ul').on('click', 'li', function (e) {
 
         $(this).toggleClass("checked");
-        // just for seperating the button from this function 
-        $(".close").click(function (event) {
-            event.stopPropagation();
 
+    })
 
-
-        });
-    });
-
-    $('ul').on('click', 'li.close', function (e) {
-        console.log("...hello " + $(this).text());
+    $('ul').on('click', '.close', function (e) {
+        // console.log("...hello " + $(this).text());
         $(this).parent().remove();
-    });
+    })
 
-
-
-
-
-})
+});
